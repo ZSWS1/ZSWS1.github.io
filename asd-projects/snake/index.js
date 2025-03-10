1e5383b6dec51a64cd155baa8f68beafba7d6124
ZSWS1 <zachswsisson@gmail.com>
2025-03-10 18:22:39 +0000

    Snake progress

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -11,6 +11,9 @@ var highScoreElement = $("#highScore");
 
 // TODO 4a: Create the snake, apple and score variables
 // Game Variables
+var snake = {}
+var apple = {}
+var score = 0
 
 // Constant Variables
 var ROWS = 20;
@@ -43,7 +46,7 @@ function init() {
   // TODO 4c-2: initialize the snake
 
   // TODO 4b-2: initialize the apple
-
+makeApple()
   // TODO 5a: Initialize the interval
 
 }
@@ -184,8 +187,24 @@ function endGame() {
  * position on the board that is not occupied and position the apple there.
  */
 function makeApple() {
-  // TODO 4b-1: Fill in the makeApple() code block
+  // TODO 4b-1: Fill in the makeApple() code block/* Create an HTML element for the apple using jQuery. Then find a random
+
+
+
+// make the apple jQuery Object and append it to the board
+apple.element = $("<div>").addClass("apple").appendTo(board);
+
+// get a random available row/column on the board
+var randomPosition = getRandomAvailablePosition();
+
+// initialize the row/column properties on the Apple Object
+apple.row = randomPosition.row;
+apple.column = randomPosition.column;
+
+// position the apple on the screen
+repositionSquare(apple);
 }
+
 
 /* Create an HTML element for a snakeSquare using jQuery. Then, given a row and
  * column on the board, position it on the screen. Finally, add the new
